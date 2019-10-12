e9e781d340aa4cfb79f6ad34692146819d1a5f16
Joshua <josh@kas.com.au>
2019-10-13 09:00:22 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const open = require('open');
+const fs = require('fs');
+
+const testFolder = './files/';
+
+const run = async () => {
+  console.log('Running...');
+
+  fs.readdirSync(testFolder).map(async file => {
+    try {
+      console.log(file);
+      await open("./files/" + file);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  
+
+  console.log('Stopped');
+};
+
+run();
